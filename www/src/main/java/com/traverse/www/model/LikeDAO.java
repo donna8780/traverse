bcd5ba7d0260b31b2fc36e779532f17c9b77fac0
minji <min10191@naver.com>
2024-08-13 10:18:25 +0900

    like_page3

---
+++ www/src/main/java/com/traverse/www/model/LikeDAO.java
@@ -0,0 +1,17 @@
+package com.traverse.www.model;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.traverse.www.vo.LikeVO;
+
+
+@Mapper
+public interface LikeDAO {
+
+   @Select("select * from like_place")
+   List<LikeVO> selectAll();
+
+}
--- www/src/main/java/com/traverse/www/vo/AccountsVO.java
+++ www/src/main/java/com/traverse/www/vo/AccountsVO.java
@@ -17,10 +17,64 @@ import lombok.Setter;
 	profile1	varchar(30)
  */
 
-@Getter
-@Setter
+
 public class AccountsVO {
 	private int accounts_idx;
 	private String userid, userpw, nick, email, name, phone, profile1;
 	private Date birth;
+	public int getAccounts_idx() {
+		return accounts_idx;
+	}
+	public void setAccounts_idx(int accounts_idx) {
+		this.accounts_idx = accounts_idx;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getUserpw() {
+		return userpw;
+	}
+	public void setUserpw(String userpw) {
+		this.userpw = userpw;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getProfile1() {
+		return profile1;
+	}
+	public void setProfile1(String profile1) {
+		this.profile1 = profile1;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	
 }
