f0fc249b23810d513b82a000afe2be646696cfdc
변재헌 <nazzang98@naver.com>
2024-08-13 10:44:34 +0900

    공지사항 게시판 css

--- www/src/main/java/com/traverse/www/controller/MypageController.java
+++ www/src/main/java/com/traverse/www/controller/MypageController.java
@@ -3,14 +3,12 @@ package com.traverse.www.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.traverse.www.service.MyPageService;
 import com.traverse.www.vo.AccountsVO;
 
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/member")
