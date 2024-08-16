65c58640d23358bf13cc85a215193a53c8641a2d
손창환 <cc0411@naver.com>
2024-08-16 09:30:16 +0900

    Api데이터 db에 추가하기

---
+++ www/src/main/java/com/traverse/www/vo/ApiVO.java
@@ -0,0 +1,94 @@
+package com.traverse.www.vo;
+
+
+public class ApiVO {
+	private int idx,area_code,sigungucode;
+	private String title,addr1,cat0,cat1,cat2,cat3,firstimage,firstimage2,tel;
+	private double mapx,mapy;
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public int getArea_code() {
+		return area_code;
+	}
+	public void setArea_code(int area_code) {
+		this.area_code = area_code;
+	}
+	public int getSigungucode() {
+		return sigungucode;
+	}
+	public void setSigungucode(int sigungucode) {
+		this.sigungucode = sigungucode;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAddr1() {
+		return addr1;
+	}
+	public void setAddr1(String addr1) {
+		this.addr1 = addr1;
+	}
+	public String getCat0() {
+		return cat0;
+	}
+	public void setCat0(String cat0) {
+		this.cat0 = cat0;
+	}
+	public String getCat1() {
+		return cat1;
+	}
+	public void setCat1(String cat1) {
+		this.cat1 = cat1;
+	}
+	public String getCat2() {
+		return cat2;
+	}
+	public void setCat2(String cat2) {
+		this.cat2 = cat2;
+	}
+	public String getCat3() {
+		return cat3;
+	}
+	public void setCat3(String cat3) {
+		this.cat3 = cat3;
+	}
+	public String getFirstimage() {
+		return firstimage;
+	}
+	public void setFirstimage(String firstimage) {
+		this.firstimage = firstimage;
+	}
+	public String getFirstimage2() {
+		return firstimage2;
+	}
+	public void setFirstimage2(String firstimage2) {
+		this.firstimage2 = firstimage2;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public double getMapx() {
+		return mapx;
+	}
+	public void setMapx(double mapx) {
+		this.mapx = mapx;
+	}
+	public double getMapy() {
+		return mapy;
+	}
+	public void setMapy(double mapy) {
+		this.mapy = mapy;
+	}
+	
+	
+}
