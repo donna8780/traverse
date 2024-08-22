4f356dc2e6138650316c80d274bdfd4e90840552
안하령 <tmxlcl0020@naver.com>
2024-08-22 14:13:53 +0900

    dgf11

---
+++ www/src/main/java/com/traverse/www/controller/TraverserStory.java
@@ -0,0 +1,11 @@
+package com.traverse.www.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TraverserStory {
+
+	@GetMapping("/place/traverserStory")
+	public void traverserStory() {}
+}
