952968d0620060f2de3ac11cd8b1d91d52c96c62
안하령 <tmxlcl0020@naver.com>
2024-08-30 22:20:07 +0900

    place_api

--- www/src/main/java/com/traverse/www/service/ApiService.java
+++ www/src/main/java/com/traverse/www/service/ApiService.java
@@ -31,7 +31,7 @@ public class ApiService {
       
       params.put("ServiceKey", "6RkKaXlTQt2IajvJnERjyEHx6pTvNp0n8ZT/iBQPLe4bXmMbm0o8mBSubhyyCBEjYC0Ur+OsD/pNUdrxNp7owQ==");
       //params.put("areaCode", "32");
-      params.put("numOfRows", "500");
+      params.put("numOfRows", "1");
       params.put("pageNo", "1");
       params.put("MobileOS", "ETC");
       params.put("MobileApp", "AppTest");
